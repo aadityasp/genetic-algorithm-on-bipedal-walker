6fe21bb0508590d7d38c3d03cdb3619bcf4d72ff
prajnan <goswami.p@northeastern.edu>
2021-03-21 01:00:06 -0400

    discount rewards by total steps in an episode

--- src/bipedal.py
+++ src/bipedal.py
@@ -18,10 +18,8 @@ import itertools
 import random
 import pprint as pp
 
-
-
-env_name=  "BipedalWalker-v3"
-env= gym.make(env_name)
+env_name =  "BipedalWalker-v3"
+env = gym.make(env_name)
 print("Observation space:", env.observation_space)
 print("Action space:", env.action_space)
 
@@ -31,16 +29,16 @@ class HillClimbingAgent():
         self.build_model()
         
     def build_model(self):
-        self.w1 = 1e-4*np.random.rand(self.state_dim, 10)
-        self.w2 = 1e-4*np.random.rand(self.state_dim, 10)
-        self.w3 = 1e-4*np.random.rand(self.state_dim, 10)
-        self.w4 = 1e-4*np.random.rand(self.state_dim, 10)
+        self.w1 = 1e-4*np.random.rand(self.state_dim, 20)
+        self.w2 = 1e-4*np.random.rand(self.state_dim, 20)
+        self.w3 = 1e-4*np.random.rand(self.state_dim, 20)
+        self.w4 = 1e-4*np.random.rand(self.state_dim, 20)
         self.best_reward = -np.Inf
         self.best_w1 = np.copy(self.w1)
         self.best_w2 = np.copy(self.w2)
         self.best_w3 = np.copy(self.w3)
         self.best_w4 = np.copy(self.w4)
-        self.noise_scale = 1e-2
+        self.noise_scale = 1e-3
         
     def get_action(self, state):
         p1 = np.dot(state, self.w1)
@@ -59,28 +57,49 @@ class HillClimbingAgent():
     # stochastic hill climbing: take random from nearest best neighbor
     def random_from_action_range(self, index):
         if index == 0:
-            return random.uniform(-1, -0.8)
+            return random.uniform(-1, -0.9)
         elif index == 1:
-            return random.uniform(-0.8, -0.6)
+            return random.uniform(-0.9, -0.8)
         elif index == 2:
-            return random.uniform(-0.6, -0.4)
+            return random.uniform(-0.8, -0.7)
         elif index == 3:
-            return random.uniform(-0.4, -0.2)
+            return random.uniform(-0.7, -0.6)
         elif index == 4:
-            return random.uniform(-0.2, 0)
+            return random.uniform(-0.6, -0.5)
         elif index == 5:
-            return random.uniform(0, 0.2)
+            return random.uniform(-0.5, -0.4)
         elif index == 6:
-            return random.uniform(0.2, 0.4)
+            return random.uniform(-0.4, -0.3)
         elif index == 7:
-            return random.uniform(0.4, 0.6)
+            return random.uniform(-0.4, -0.3)
         elif index == 8:
-            return random.uniform(0.6, 0.8)
+            return random.uniform(-0.3, -0.2)
         elif index == 9:
-            return random.uniform(0.8, 1)
+            return random.uniform(-0.2, -0.1)
+        elif index == 10:
+            return random.uniform(-0.1, 0)
+        elif index == 11:
+            return random.uniform(0, 0.1)
+        elif index == 12:
+            return random.uniform(0.1, 0.2)
+        elif index == 13:
+            return random.uniform(0.3, 0.4)
+        elif index == 14:
+            return random.uniform(0.4, 0.5)
+        elif index == 15:
+            return random.uniform(0.5, 0.6)
+        elif index == 16:
+            return random.uniform(0.6, 0.7)
+        elif index == 17:
+            return random.uniform(0.7, 0.8)
+        elif index == 18:
+            return random.uniform(0.8, 0.9)
+        elif index == 19:
+            return random.uniform(0.0, 1)
     
     def update_model(self, reward):
         if reward >= self.best_reward:
+            print ("higher reward")
             self.best_reward = reward
             self.best_w1 = np.copy(self.w1)
             self.best_w2 = np.copy(self.w2)
@@ -88,28 +107,34 @@ class HillClimbingAgent():
             self.best_w4 = np.copy(self.w4)
             self.noise_scale = max(self.noise_scale/2, 1e-3)
         else:
+            print ("lower reward")
             self.noise_scale = min(self.noise_scale*2, 2)
             
-        self.w1 = self.best_w1 + self.noise_scale * np.random.rand(self.state_dim, 10)
-        self.w2 = self.best_w2 + self.noise_scale * np.random.rand(self.state_dim, 10)
-        self.w3 = self.best_w3 + self.noise_scale * np.random.rand(self.state_dim, 10)
-        self.w4 = self.best_w4 + self.noise_scale * np.random.rand(self.state_dim, 10)
+        self.w1 = self.best_w1 + self.noise_scale * np.random.rand(self.state_dim, 20)
+        self.w2 = self.best_w2 + self.noise_scale * np.random.rand(self.state_dim, 20)
+        self.w3 = self.best_w3 + self.noise_scale * np.random.rand(self.state_dim, 20)
+        self.w4 = self.best_w4 + self.noise_scale * np.random.rand(self.state_dim, 20)
+
 
 agent = HillClimbingAgent(env)
-num_episodes = 100
+num_episodes = 1000
 
 for ep in range(num_episodes):
     state = env.reset()
     total_reward = 0
     done = False
+    steps = 0
     while not done:
         action = agent.get_action(state)
         #print ("Action: ", action)
         state, reward, done, info = env.step(action)
+        if bool(info) == True:
+            print ("debug: ", info)
         env.render()
+        steps += 1
         total_reward += reward
         
-    agent.update_model(total_reward)
+    agent.update_model(total_reward/steps)
     print("Episode: {}, total_reward: {:.2f}".format(ep, total_reward))
 
 env.close()
